c0a54b4eb6f287f33840ebb73429ba31d0270420
fedelaurna <142011660+fedelaurna@users.noreply.github.com>
2025-09-10 08:18:37 -0300

    Add files via upload

---
+++ apodo.js
@@ -0,0 +1,32 @@
+/* let contenedor = document.querySelector('.contenedor')
+
+fetch('https://api.nasa.gov/planetary/apod?api_key=emAX7kURdpbL53UttDOLM2dhrthdEzYY3BtUbdhG&count=3')
+
+.then(respuesta => respuesta.json())
+.then(datos => {
+    console.log(datos)
+    //img.src = datos[0].url
+    for (let i = 0; i < datos.length; i++) {
+        //const element = array[index];
+        contenedor.innerHTML += '<h1>'+datos[i].title+'</h1><img src="'+datos[i].url+'" alt"">' 
+    }
+})
+
+
+
+
+
+fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+.then(res => res.json())
+.then(datos => {
+   const contenedor = document.querySelector("#contenedor");
+
+
+   contenedor.innerHTML =
+   <h2>${ datos.title} </h2>
+   <p>  </p>
+
+   ;
+});
+
+*/
